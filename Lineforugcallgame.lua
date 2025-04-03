e4181e51e19c12043701e86256f0ebdbdfe7f644
AsLikeJustScriptLuaForRoblox <hilosarry0@gmail.com>
2025-04-03 22:08:37 +0700

    Update Lineforugcallgame.lua

--- Lineforugcallgame.lua
+++ Lineforugcallgame.lua
@@ -11,7 +11,7 @@ pcall(function()
         while wait() do
             if getgenv().AutoLine then
                 local args = {
-                    [1] = 5000
+                    [1] = 10000
                 }
 
                 game:GetService("ReplicatedStorage"):WaitForChild("foreverpack"):WaitForChild("Claim"):FireServer(unpack(args))
@@ -29,7 +29,7 @@ pcall(function()
 
                 if #game:GetService("Players"):GetPlayers() <= 1 then
                     game:GetService("Players").LocalPlayer:Kick("\nRejoining...")
-                    wait(0.1) 
+                    wait(1)
                     game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
                 else
                     game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game:GetService("Players").LocalPlayer)
