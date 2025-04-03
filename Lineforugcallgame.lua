1c0c7016d1e08d82f944909500500c736bf504de
AsLikeJustScriptLuaForRoblox <hilosarry0@gmail.com>
2025-04-03 22:04:03 +0700

    Update Lineforugcallgame.lua

--- Lineforugcallgame.lua
+++ Lineforugcallgame.lua
@@ -11,7 +11,7 @@ pcall(function()
         while wait() do
             if getgenv().AutoLine then
                 local args = {
-                    [1] = 2000
+                    [1] = 3000
                 }
 
                 game:GetService("ReplicatedStorage"):WaitForChild("foreverpack"):WaitForChild("Claim"):FireServer(unpack(args))
